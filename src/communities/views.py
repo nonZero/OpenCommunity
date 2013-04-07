81b87f1ec63ff0068ce5824cf1f8d649196e37e4
Udi Oron <udioron@gmail.com>
2013-04-07 14:09:58 +0300

    added temp redirect to issues

--- src/communities/views.py
+++ src/communities/views.py
@@ -1,15 +1,18 @@
 from communities import models
+from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
-from django.views.generic.detail import DetailView
-#from django.views.generic.base import RedirectView
+from django.views.generic.base import RedirectView
 
 
 class CommunityList(ListView):
     model = models.Community
 
 
-class CommunityDetailView(DetailView):
-    model = models.Community
+#class CommunityDetailView(DetailView):
+    #model = models.Community
 
-#class CommunityDetailView(RedirectView):
-#    model = models.Community
+class CommunityDetailView(RedirectView):
+    def get_redirect_url(self, **kwargs):
+        self.community = get_object_or_404(models.Community, pk = self.kwargs['pk'])
+        return reverse('issues', args=(str(self.community.id),))
