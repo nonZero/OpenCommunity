3c52fc3963473257c46046d76e3995c2a5cd5d35
Udi Oron <udioron@gmail.com>
2013-04-10 00:15:38 +0300

    Added "new proposal" form

--- src/issues/models.py
+++ src/issues/models.py
@@ -81,3 +81,7 @@ class Proposal(models.Model):
     def __unicode__(self):
         return self.title
 
+    @models.permalink
+    def get_absolute_url(self):
+        return ("issue", (str(self.issue.community.pk), str(self.issue.pk), ))
+
--- src/issues/views.py
+++ src/issues/views.py
@@ -4,16 +4,18 @@ from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from issues import models
-from issues.forms import CreateIssueForm
+from issues.forms import CreateIssueForm, CreateProposalForm
 # from django.views.generic.base import RedirectView
 
 
-class IssueMixin(object):
+class CommunityMixin(object):
 
     @property
     def community(self):
         return get_object_or_404(Community, pk=self.kwargs['community_id'])
 
+
+class IssueMixin(CommunityMixin):
     def get_queryset(self):
         return models.Issue.objects.filter(community=self.community)
 
@@ -42,6 +44,28 @@ class IssueCreateView(IssueMixin, CreateView):
         form.instance.community = self.community
         form.instance.created_by = self.request.user
         return super(IssueCreateView, self).form_valid(form)
+
+
+class ProposalCreateView(IssueMixin, CreateView):
+
+    model = models.Proposal
+    form_class = CreateProposalForm
+
+    @property
+    def issue(self):
+        return get_object_or_404(models.Issue, community=self.community, pk=self.kwargs['pk'])
+
+    def get_context_data(self, **kwargs):
+        context = super(ProposalCreateView, self).get_context_data(**kwargs)
+
+        context['issue'] = self.issue
+
+        return context
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        form.instance.issue = self.issue
+        return super(ProposalCreateView, self).form_valid(form)
 
 
 # class CommunityDetailView(RedirectView):
