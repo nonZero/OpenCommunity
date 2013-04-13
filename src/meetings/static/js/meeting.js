4aad313bfeb8f31468920201ef9c360ddb89e41d
Udi Oron <udioron@gmail.com>
2013-04-14 00:10:21 +0300

    added ajax loading indicator

--- src/meetings/static/js/meeting.js
+++ src/meetings/static/js/meeting.js
@@ -2,13 +2,13 @@
 
 $(function() {
    $('.issues li').click(function() {
-       var li = $(this);
+       var li = $(this).addClass('loading');
        $.post('', {
            issue: li.data('issue'),
            set: li.data('set')
            }, function(data) {
                console.log(li);
-               li.data('set', data).attr('data-set', data);
+               li.data('set', data).attr('data-set', data).removeClass('loading');
                console.log(li.data('set'));
            });
    }) 
