c679440e2a32765a5f5bf10ca3befffbb9b18a9e
Udi Oron <udioron@gmail.com>
2013-04-23 22:20:46 +0300

    added windows setup batch file

---
+++ windows-setup.cmd
@@ -0,0 +1,22 @@
+@echo off
+
+rem echo ===============================
+rem echo Installing PIL
+rem easy_install http://effbot.org/downloads/PIL-1.1.7.win32-py2.7.exe
+rem echo ===============================
+
+echo ===============================
+echo Installing win-psycopg
+easy_install http://www.stickpeople.com/projects/python/win-psycopg/2.5.0/psycopg2-2.5.win32-py2.7-pg9.2.4-release.exe
+echo ===============================
+
+rem easy_install lxml==2.3
+rem echo ===============================
+rem echo ===============================
+
+rem echo ===============================
+rem echo Installing ipython
+rem pip install ipython
+rem echo ===============================
+
+rem pip freeze
