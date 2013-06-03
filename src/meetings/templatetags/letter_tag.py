1e6faaac1ffb41ab43c8af59f75db4b1e0c7b49e
Udi Oron <udioron@gmail.com>
2013-06-03 16:33:15 +0300

    minor tweaks to meeting template

--- src/meetings/templatetags/letter_tag.py
+++ src/meetings/templatetags/letter_tag.py
@@ -1,13 +1,14 @@
 from django import template
 from django.utils.translation import ugettext_lazy as _
 
-
-# Just to get hebrew letters translated, not is any other use elsewhere.
-LETTERS = [_("a"),_("b"),_("c"),_("d"),_("e"),_("f"),_("g"),_("h"),_("i"),_("j")]
-
+LETTERS = [_("a"), _("b"), _("c"), _("d"), _("e"),
+           _("f"), _("g"), _("h"), _("i"), _("j")]
 
 register = template.Library()
 
+
 @register.filter
 def to_char(value):
-    return _(chr(96+value))
+    if value > len(LETTERS):
+        return value
+    return LETTERS[value - 1]
