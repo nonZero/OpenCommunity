5c9c030bbe756cbaeed09aae75b2214f03f3801c
Udi Oron <udioron@gmail.com>
2013-06-09 11:16:41 +0300

    updated fabric deploy script

--- src/fabfile.py
+++ src/fabfile.py
@@ -1,7 +1,14 @@
 from fabric.api import *
 from local_fab import code_dir, my_hosts
+from contextlib import contextmanager as _contextmanager
 
 env.hosts = my_hosts
+
+@_contextmanager
+def virtualenv(path):
+    with cd(path):
+        with prefix('source bin/activate'):
+            yield
 
 
 def host_type():
@@ -9,8 +16,10 @@ def host_type():
 
 
 def deploy():
-    with cd(code_dir):
+    with virtualenv(code_dir):
         run("git pull")
+        run("cd src && python manage.py migrate")
+        run("cd src && python manage.py collectstatic --noinput")
         run("sudo supervisorctl restart opencommunity")
 
 
