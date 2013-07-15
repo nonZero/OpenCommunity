7f38186c4a0bb2202bea3a3ec5a2d34b68f49822
Udi Oron <udioron@gmail.com>
2013-07-15 15:33:34 +0300

    better fabric deploy script

--- src/fabfile.py
+++ src/fabfile.py
@@ -1,13 +1,25 @@
 from fabric.api import *
-from local_fab import code_dir, my_hosts
 from contextlib import contextmanager as _contextmanager
 
-env.hosts = my_hosts
+env.code_dir = '~/OpenCommunity/'
+env.venv_command = '. bin/activate'
+env.log_dir = '/var/log/opencommunity/'
+
+
+def qa():
+    env.hosts = ['udi@oc-dev.modelarity.com']
+    env.log_dir = '%slogs/' % env.code_dir
+
+
+def prod():
+    env.hosts = ['oc@ny1.opencommunity.org.il']
+    env.venv_command = '. ~/.virtualenvs/oc/bin/activate'
+
 
 @_contextmanager
 def virtualenv(path):
     with cd(path):
-        with prefix('source bin/activate'):
+        with prefix(env.venv_command):
             yield
 
 
@@ -15,14 +27,24 @@ def host_type():
     run('uname -s')
 
 
+def freeze():
+    with virtualenv(env.code_dir):
+        run("pip freeze")
+
+
 def deploy():
-    with virtualenv(code_dir):
+    with virtualenv(env.code_dir):
         run("git pull")
         run("pip install -r requirements.txt")
+        run("pip install -r deploy-requirements.txt")
         run("cd src && python manage.py migrate")
         run("cd src && python manage.py collectstatic --noinput")
-        run("sudo supervisorctl restart opencommunity")
+        run("cd src && kill -HUP `cat masterpid`")
+
+
+def hard_reload():
+    run("sudo supervisorctl restart opencommunity")
 
 
 def log():
-    run("tail %slogs/*" % code_dir)
+    run("tail %s*" % env.log_dir)
