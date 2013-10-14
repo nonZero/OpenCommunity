ee61b222546dba660eb995145fc68bfc8e85ceef
Udi Oron <udioron@gmail.com>
2013-10-14 15:38:26 +0300

    working on a new crazy fabfile

--- server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source /home/oc/.virtualenvs/oc/bin/activate
-cd $DIR/src
-exec gunicorn -p masterpid -b 127.0.0.1:9000 -w 2 ocd.wsgi:application
