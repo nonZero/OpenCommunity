5672a4e84c87c9b9a09292fe2206fe0c756e4c85
Udi Oron <udi@agala.(none)>
2013-10-22 17:45:22 +0300

    prevent double clicking on proposal accept/reject/cancel

--- src/issues/static/js/proposal.js
+++ src/issues/static/js/proposal.js
@@ -1,29 +1,39 @@
 "use strict";
-
-function unacceptProposal(id, value) {
-    $.post('', {
-        issue : id,
-        accepted : value,
-        unaccept: '1',
-    }, function(data) {
-        history.back();
-    });
-}
-
-
 $(function() {
+
+    // disclaimer: this code is ugly.
+
+    var sent = false;
+
+    function setProposalStatus(id, value) {
+        if (sent) {
+            return;
+        }
+        sent = true;
+        $(".accept-buttons,.unaccept").addClass('disabled');
+        $.post('', {
+            issue : id,
+            accepted : value,
+            unaccept: '1',
+        }, function(data) {
+            history.back();
+        });
+    }
+    
+    
+
 
     $(".proposal-action").click(function(event) {
         event.preventDefault();
         var el = $(this).closest('.proposal');
-        unacceptProposal(el.data('id'), $(this).val());
+        setProposalStatus(el.data('id'), $(this).val());
         return false;
     });
 
     $(".unaccept").click(function(event) {
         event.preventDefault();
         var el = $(this).closest('.proposal');
-        unacceptProposal(el.data('id'), $(this).data('value'));
+        setProposalStatus(el.data('id'), $(this).data('value'));
         return false;
     });
 
