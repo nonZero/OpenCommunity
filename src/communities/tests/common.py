10942f8c02f4f5fb1d2e3db570d4e5f74ba89b47
Alon <Illniyar@gmail.com>
2013-11-08 21:34:33 +0200

    added tests for invitations

---
+++ src/communities/tests/common.py
@@ -0,0 +1,27 @@
+from users.models import OCUser, Membership
+from communities.models import Community
+from collections import namedtuple
+from users.default_roles import DefaultGroups
+
+def createTestCommunity(roles,community_name="Foo Inc.",email_template=None):
+    community = Community.objects.create(name="Foo Inc.")
+    members = []
+    chairmen=[]
+    if not email_template:
+        words = community_name.lower().split(" ,-_.")
+        email_template = words[0] + "%d@" + ".".join(words) 
+    for i, g in enumerate(roles):
+        u = OCUser.objects.create_user(email_template % i,
+                                       "%s %d" % (community_name,i),password="password")
+        Membership.objects.create(user=u, community=community, default_group_name=g)
+        if g == DefaultGroups.CHAIRMAN:
+            chairmen.append(u)
+        members.append(u)
+    return namedtuple('Result',['community','members','chairmen'])(community,members,chairmen)
+
+def createSampleCommunity():
+    return createTestCommunity(roles = (
+                 [DefaultGroups.CHAIRMAN] +
+                 [DefaultGroups.SECRETARY] * 2 +
+                 [DefaultGroups.BOARD] * 5 +
+                 [DefaultGroups.MEMBER] * 10))
---
+++ src/users/tests/invitation_test.py
@@ -0,0 +1,98 @@
+from communities.models import  SendToOption
+from django.core import mail
+from django.test import TestCase
+from users.default_roles import DefaultGroups
+from users.models import Invitation, Membership, OCUser
+from communities.tests.common import createSampleCommunity
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext  as _
+from django.test.utils import override_settings
+
+class InvitationTest(TestCase):
+    def setUp(self):
+        (self.community,self.members,self.chairmens) = createSampleCommunity()  
+        
+    def tearDown(self):
+        mail.outbox = []    
+    def test_send_invitation(self):
+
+        i = Invitation.objects.create(community=self.community,
+                                             created_by=self.members[0],
+                                             email="xxx@xyz.com")
+        i.send()
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertIn(self.community.name, mail.outbox[0].subject)  
+       
+class InvitationViewTest(TestCase):
+
+    def post_invite(self,data= None):
+        if not data:
+            data = {"email":"sample@email.com",
+                                              "default_group_name":DefaultGroups.MEMBER,
+                                              "message":"the message"}
+        return self.client.post(reverse("members"
+                          ,kwargs={"community_id":self.community.id}),
+                          data)
+    def login_chairmen(self):
+        self.client.login(username=self.chairmen[0].email,password="password")
+    
+    def setUp(self):        
+        (self.community,self.members,self.chairmen) = createSampleCommunity()
+        
+    def tearDown(self):
+        mail.outbox = []    
+         
+    def test_view(self):
+        self.login_chairmen()
+        response = self.post_invite({"email":"sample@email.com",
+                                              "default_group_name":DefaultGroups.MEMBER,
+                                              "message":"the message"})
+        self.assertEqual(Invitation.objects.all().count(),1)
+        invitation = Invitation.objects.all()[0]
+        self.assertEqual(invitation.community,self.community)
+        self.assertEqual(invitation.created_by,self.chairmen[0])
+        self.assertEqual(invitation.message,"the message")
+        
+        self.assertEqual(len(mail.outbox), 1)
+        
+        self.assertEqual(response.status_code,200)
+        #the response is an ajax response the show the user as added
+        #to the list of members
+        self.assertIn("delete-invitation",response.content) 
+        self.assertIn("sample@email.com",response.content)
+        
+    
+    def test_no_invite_permission(self):
+        self.client.login(username=self.members[6].email,password="password")
+        response = self.post_invite()
+        self.assertEqual(response.status_code,403)
+        self.assertEqual(len(mail.outbox), 0)
+        self.assertEqual(Invitation.objects.all().count(),0)
+                
+    def test_bad_email(self):
+        self.login_chairmen()
+        response = self.post_invite({"email":"not a real email",
+                                              "default_group_name":DefaultGroups.MEMBER,
+                                              "message":"the message"})
+        self.assertEqual(response.status_code,400)
+        self.assertEqual(_("Form error. Please supply a valid email."),response.content)
+               
+    def test_invitee_already_invited(self):
+        Invitation.objects.create(community=self.community,
+                                             created_by=self.chairmen[0],
+                                             email="sample@email.com")
+        self.login_chairmen()
+        response = self.post_invite()  
+        self.assertEqual(response.status_code,400)
+        self.assertEqual(_("This user is already invited to this community."),response.content)
+
+    def test_invitee_already_a_member(self):
+        u = OCUser.objects.create_user("sample@email.com",
+                                        "sample user",password="password")
+        Membership.objects.create(user=u, community=self.community, default_group_name=DefaultGroups.MEMBER)
+        self.login_chairmen()
+
+        response = self.post_invite()    
+        self.assertEqual(response.status_code,400)    
+        self.assertEqual(_("This user already a member of this community."),response.content)
