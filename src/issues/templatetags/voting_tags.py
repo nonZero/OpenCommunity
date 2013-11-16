19d7f02f6ed7a17a55388737e61319a25fcf093e
Yaniv Mirel <yanivmirel@gmail.com>
2013-11-17 00:11:54 +0200

    Merge with Amir fix for voting issues. Styled voting in issue page inside proposal white area. Switched cancle/save comments edit.

--- src/issues/templatetags/voting_tags.py
+++ src/issues/templatetags/voting_tags.py
@@ -30,17 +30,8 @@ def user_votes_on_issue(context):
     votes_cnt = ProposalVote.objects.filter(
         proposal__in=proposals,
         user_id=user_id).count()
-    return "<span class='votes_count'>{0}</span>/<span class='proposal_count'>{1}</span>".format(votes_cnt, proposals.count())
-
-
-@register.filter
-def vote_percentage(proposal):
-    if proposal.votes_pro is None:
-        return 'undefined'
-    votes = proposal.votes_pro + proposal.votes_con
-    print votes ,proposal.community_members
-    percentage = (float(votes) / float(proposal.community_members)) * 100.0  
-    return str(percentage)
+#     return "<span class='votes_count'>{0}</span>/<span class='proposal_count'>{1}</span>".format(votes_cnt, proposals.count())
+    return "{0}/{1}".format(votes_cnt, proposals.count())
 
 
 @register.filter    
