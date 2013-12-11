98dbad4ccbe8311d9e5ad0f66b36683be26a0765
Yaniv Mirel <yanivmirel@gmail.com>
2013-12-12 01:13:55 +0200

    More responsive

--- src/meetings/models.py
+++ src/meetings/models.py
@@ -94,6 +94,9 @@ class Meeting(UIDMixin):
     def get_title_or_date(self):
         return self.title or date_format(self.held_at)
  
+    def get_title_or_shortdate(self):
+        return self.title or self.held_at.strftime('%d/%m/%Y')
+ 
     @models.permalink
     def get_absolute_url(self):
         return ("meeting", (str(self.community.pk), str(self.pk),))
