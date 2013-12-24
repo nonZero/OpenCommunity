87ccb02ca3d7dcd3c367148d2f0a2407d6224ed4
Udi Oron <udioron@gmail.com>
2013-12-24 14:33:49 +0200

    Fixedfilter by tabs

--- src/ocd/static/js/filter.js
+++ src/ocd/static/js/filter.js
@@ -1,73 +1,35 @@
-/* ========================================================================
- * Filter: filter.js v0.1
- * ======================================================================== */
+"use strict";
 
-+ function($) {"use strict";
+/**
+ * Toggles visibilty for titles with no "child" elements.
+ */
+function toggleTitles() {
+    $('.filter-title').each(function() {
+        $(this).toggle($(this).next().find('.filter:visible').length > 0);
+    });
+}
 
-    // Filter PUBLIC CLASS DEFINITION
-    // ================================
-
-    var Filter = function(element, options) {
-        this.$element = $(element);
-        this.options = $.extend({}, Filter.DEFAULTS, options);
-    };
-
-    Filter.DEFAULTS = {
-    };
-
-    Filter.prototype.activate = function() {
-        this.$element.closest('ul').children('li').removeClass('active');
-        this.$element.parent('li').addClass('active');
-        if (this.options.hide) {
-            $(this.options.hide).hide().removeClass('on');
-        }
-        if (this.options.show) {
-            $(this.options.show).show().addClass('on');
-        }
-        $('.filter-title').each(function() {
-            $(this).toggle(
-                $(this).next().find('.filter:visible').length > 0
-            );
-        });
-    };
-
-    // FILTER PLUGIN DEFINITION
-    // ==========================
-
-    var old = $.fn.filter;
-
-    $.fn.filter = function(option) {
-        return this.each(function() {
-            var $this = $(this);
-            var data = $this.data('oc.filter');
-            var options = $.extend({}, Filter.DEFAULTS, $this.data(), typeof option == 'object' && option);
-
-            if (!data) {
-                $this.data('oc.filter', ( data = new Filter(this, options)));
-            }
-            data[typeof option == 'string' ? option : 'activate']();
-        });
-    };
-
-    $.fn.filter.Constructor = Filter;
-
-    // COLLAPSE NO CONFLICT
-    // ====================
-
-    $.fn.filter.noConflict = function() {
-        $.fn.filter = old;
-        return this;
-    };
-
-    // COLLAPSE DATA-API
-    // =================
+$(function() {
 
     $(document).on('click.filter.data-api', '[data-toggle=filter]', function(e) {
+
         var $this = $(this);
-        var data = $this.data('oc.filter');
-        var options = data ? 'activate' : $this.data();
-        $this.filter(options);
-        return false;
+
+        var root = $this.closest('[data-filter]');
+
+        root.find('li').removeClass('active');
+        $this.parent('li').addClass('active');
+        $this.closest('li.dropdown').addClass('active');
+        if ($this.data('show')) {
+            $(root.data('filter')).hide().removeClass('on');
+            $($this.data('show')).show().addClass('on');
+        } else {
+            $(root.data('filter')).show().addClass('on');
+        }
+
+        toggleTitles();
+
     });
 
-}(window.jQuery);
+    toggleTitles();
+});
