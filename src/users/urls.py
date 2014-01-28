3f570a542b762b7bc83e3910a9d149237b924735
Yaniv Mirel <yanivmirel@gmail.com>
2014-01-28 17:17:02 +0200

    Fixed error when trying to delete invitation

--- src/users/urls.py
+++ src/users/urls.py
@@ -5,7 +5,7 @@ from users import views
 urlpatterns = patterns('',
 
     url(r'^$', views.MembershipList.as_view(), name="members"),
-    url(r'^(?P<pk>\d+)/', views.MemberProfile.as_view(), name="member_profile"),
+    url(r'^(?P<pk>\d+)/$', views.MemberProfile.as_view(), name="member_profile"),
     url(r'^(?P<pk>\d+)/delete-invitation/$', views.DeleteInvitationView.as_view(), 
         name="delete_invitation"),
     url(r'^autocomp/$', views.AutocompleteMemberName.as_view(), name="ac_user"),
