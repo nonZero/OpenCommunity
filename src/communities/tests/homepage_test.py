97eb8b483b557db0198ff4b7e534ee0c985b5819
Udi Oron <udioron@gmail.com>
2014-03-19 17:44:47 +0200

    Refactored homepage test

--- src/communities/tests/homepage_test.py
+++ src/communities/tests/homepage_test.py
@@ -1,11 +1,11 @@
-import logging
 from django.contrib.auth import get_user_model
-
 from django.core.urlresolvers import reverse
-from django.test.client import Client
 from django.test.testcases import TestCase
+
 from users.default_roles import DefaultGroups
 from users.models import Membership
+
+PASSWORD = 'secret'
 
 User = get_user_model()
 
@@ -14,38 +14,37 @@ from communities.models import Community
 
 class CommunitiesTest(TestCase):
     def setUp(self):
-        self.c1 = Community.objects.create(name='Public Community XYZZY', is_public=True)
-        self.c2 = Community.objects.create(name='Private Community ABCDE', is_public=False)
+        self.c1 = Community.objects.create(name='Public Community XYZZY',
+                                           is_public=True)
+        self.c2 = Community.objects.create(name='Private Community ABCDE',
+                                           is_public=False)
         self.assertEquals(2, Community.objects.count())
 
-        self.not_a_member = User.objects.create_user('foo@gmail.com', 'Foo', 'secret')
-        self.c2member = User.objects.create_user('bar@gmail.com', 'Bar', 'secret')
-        Membership.objects.create(community=self.c2, user=self.c2member, default_group_name=DefaultGroups.MEMBER)
+        self.not_a_member = User.objects.create_user(
+            'foo@gmail.com', 'Foo', PASSWORD)
+        self.c2member = User.objects.create_user(
+            'bar@gmail.com', 'Bar', PASSWORD)
 
-        self.client = Client()
-
-
-    def tearDown(self):
-        pass
+        Membership.objects.create(community=self.c2, user=self.c2member,
+                                  default_group_name=DefaultGroups.MEMBER)
 
 
-    def test_homepage_annonymous(self):
+    def visit_home(self, user=None):
+        if user:
+            self.client.login(email=user.email, password=PASSWORD)
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, self.c1.name)
+        return response
+
+    def test_homepage_annonymous(self):
+        response = self.visit_home()
         self.assertNotContains(response, self.c2.name)
 
     def test_homepage_member(self):
-        self.client.login(email=self.c2member.email, password='secret')
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.c1.name)
+        response = self.visit_home(self.c2member)
         self.assertContains(response, self.c2.name)
 
     def test_homepage_not_a_member(self):
-        self.client.login(email=self.not_a_member.email, password='secret')
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.c1.name)
+        response = self.visit_home(self.not_a_member)
         self.assertNotContains(response, self.c2.name)
-
