2684ba3897cd8d2eb92bc9d1909052234ef56957
Udi Oron <udioron@gmail.com>
2014-03-26 15:27:56 +0200

    Refactored community tests

--- src/communities/tests/common.py
+++ src/communities/tests/common.py
@@ -1,7 +1,14 @@
-from users.models import OCUser, Membership
-from communities.models import Community
 from collections import namedtuple
+
+from django.contrib.auth import get_user_model
+
+from communities.models import Community
 from users.default_roles import DefaultGroups
+from users.models import OCUser, Membership
+
+PASSWORD = 'secret'
+
+User = get_user_model()
 
 
 def create_community(roles, community_name="Foo Inc.", email_template=None):
@@ -13,12 +20,15 @@ def create_community(roles, community_name="Foo Inc.", email_template=None):
         email_template = words[0] + "%d@" + ".".join(words)
     for i, g in enumerate(roles):
         u = OCUser.objects.create_user(email_template % i,
-                                       "%s %d" % (community_name, i), password="password")
-        Membership.objects.create(user=u, community=community, default_group_name=g)
+                                       "%s %d" % (community_name, i),
+                                       password="password")
+        Membership.objects.create(user=u, community=community,
+                                  default_group_name=g)
         if g == DefaultGroups.CHAIRMAN:
             chairmen.append(u)
         members.append(u)
-    return namedtuple('Result', ['community', 'members', 'chairmen'])(community, members, chairmen)
+    return namedtuple('Result', ['community', 'members', 'chairmen'])(
+        community, members, chairmen)
 
 
 def create_sample_community():
@@ -27,3 +37,37 @@ def create_sample_community():
         [DefaultGroups.SECRETARY] * 2 +
         [DefaultGroups.BOARD] * 5 +
         [DefaultGroups.MEMBER] * 10))
+
+
+class CommunitiesMixin(object):
+    @classmethod
+    def create_member(cls, username, community=None):
+        u = User.objects.create_user(
+            '{}@gmail.com'.format(username), username.title(), PASSWORD)
+        if community:
+            Membership.objects.create(community=community, user=u,
+                                      default_group_name=DefaultGroups.MEMBER)
+        return u
+
+    @classmethod
+    def setUpClass(cls):
+        super(CommunitiesMixin, cls).setUpClass()
+        cls.c1 = Community.objects.create(name='Public Community XYZZY',
+                                          is_public=True)
+        cls.c2 = Community.objects.create(name='Private Community ABCDE',
+                                          is_public=False)
+
+        cls.not_a_member = cls.create_member("foo")
+        cls.c1member = cls.create_member("bar", cls.c1)
+        cls.c2member = cls.create_member("baz", cls.c2)
+
+
+    def visit(self, url, user=None, success=True):
+        client = self.client_class()
+        if user:
+            client.login(email=user.email, password=PASSWORD)
+        response = client.get(url)
+        self.assertEqual(response.status_code,
+                         200 if success else (403 if user else 302))
+        return response
+
--- src/communities/tests/smoke_test.py
+++ src/communities/tests/smoke_test.py
@@ -1,48 +1,10 @@
-from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse
 from django.test.testcases import TestCase
 
-from users.default_roles import DefaultGroups
-from users.models import Membership
-
-PASSWORD = 'secret'
-
-User = get_user_model()
-
-from communities.models import Community
+from communities.tests.common import CommunitiesMixin
 
 
-class CommunitiesTest(TestCase):
-    @classmethod
-    def create_member(cls, username, community=None):
-        u = User.objects.create_user(
-            '{}@gmail.com'.format(username), username.title(), PASSWORD)
-        if community:
-            Membership.objects.create(community=community, user=u,
-                                      default_group_name=DefaultGroups.MEMBER)
-        return u
-
-    @classmethod
-    def setUpClass(cls):
-        cls.c1 = Community.objects.create(name='Public Community XYZZY',
-                                          is_public=True)
-        cls.c2 = Community.objects.create(name='Private Community ABCDE',
-                                          is_public=False)
-
-        cls.not_a_member = cls.create_member("foo")
-        cls.c1member = cls.create_member("bar", cls.c1)
-        cls.c2member = cls.create_member("baz", cls.c2)
-
-
-    def visit(self, url, user=None, success=True):
-        client = self.client_class()
-        if user:
-            client.login(email=user.email, password=PASSWORD)
-        response = client.get(url)
-        self.assertEqual(response.status_code,
-                         200 if success else (403 if user else 302))
-        return response
-
+class CommunitiesTest(CommunitiesMixin, TestCase):
     def visit_home(self, user=None):
         response = self.visit(reverse('home'), user)
         self.assertContains(response, self.c1.name)
