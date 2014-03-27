d31305cd5461c7456d6014ce63a06e5efd1da38f
Udi Oron <udioron@gmail.com>
2014-03-27 22:58:10 +0200

    Trying to fix garbled emails, now django 1.5 style

--- src/oc_util/email_util.py
+++ src/oc_util/email_util.py
@@ -5,10 +5,13 @@ from django.core.mail.message import EmailMultiAlternatives
 
 def fix_garbled_mail():
     """ 8bit seems to cause buggy emails in Hebrew.  revert back to base64"""
-    from email.charset import BASE64
-    from django.core.mail.message import utf8_charset
+    # In django 1.5, this prevents BASE64:
+    from django.core.mail import message
+    # let's undo it:
+    from email import Charset
+    Charset.add_charset('utf-8', Charset.SHORTEST, Charset.BASE64, 'utf-8')
+    # utf8_charset.body_encoding = Charset.BASE64  # Django 1.6
 
-    utf8_charset.body_encoding = BASE64
 
 
 def send_mails(from_email, emails, subject, message, html_message=None,
